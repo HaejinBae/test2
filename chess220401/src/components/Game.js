3ccb84f705d5a75d872795fbd843474d3e36706d
DESKTOP-MK3H9P5\admin <kuppernickel@gmail.com>
2022-05-13 17:47:47 +0900

    코드 정리+ 주석 추가함 이동 가능한 칸을 계산하고 저장할 2차원 배열 필요 말 이동 규칙 계산할 함수 필요

--- chess220401/src/components/Game.js
+++ chess220401/src/components/Game.js
@@ -99,15 +99,14 @@ export default class Game extends React.Component{
     }
 
     selectedPiece(p){
-        
-        let s='';
+        //현재 선택한 칸의 좌표를 저장할 변수
         let po=[];
-        let cmp='';
+        //html상에 표시된 선택된 칸의 정보 불러오기
         let sval=document.getElementById('sval').innerHTML;
         let spos=document.getElementById('spos').innerHTML;
         let scmp=document.getElementById('scmp').innerHTML;
-        let mov;
         
+        //현재 선택한 칸의 정보를 저장, 아래에서 html상에 표시(정보 임시 저장용)
         this.setState({
             history: this.state.history,
             selected:p,
@@ -115,30 +114,38 @@ export default class Game extends React.Component{
         });
         // this.state.selected=p; //원인?
         
-        po=p[1];
+        //말 선택
+        //말이 있는 칸의, 현재 차례와 맞는 진영의 말 선택 시 진영 정보를 비워 둠
+        //이동할 칸 클릭 시 차례와 맞는 진영의 칸이 아니므로 동작하지 않음
         if(p[0]!=''&&p[2]==this.state.next){ 
-            s=p[0];
-            sval=s;
-            spos=po;
-            scmp=cmp;
+            // sval=p[0]; //선택한 말의 표시값
+            // spos=p[1]; //선택한 말의 좌표
+            scmp=''; //말 선택과 동시에 아래가 동작하지 않도록 진영을 비움
+            // console.log(sval+","+spos+","+scmp);
         }
-        if(this.state.next==scmp&&sval!=''){ //현재 차레 플레이어와 같은 진영의 말 선택 시, 말이 있는(value가 존재하는) 칸만 이동 가능
+        //말 이동
+        //선택된 말이 현재 차레 플레이어와 같은 진영일 때 클릭한 칸으로 이동 가능
+        //위의 동작으로 인해 선택된 말의 진영이 비워져있으므로 말 선택 시에는 동작하지 않음
+        //이동할 칸 클릭시 html상에 출력된 진영 정보를 읽어 와 현재 차례와 비교
+        if(this.state.next==scmp){ 
+            po=p[1];
+
             console.log(sval+","+spos);
-            this.state.history[0].squares[po[0]][po[1]].value=sval;
-            this.state.history[0].squares[spos[0]][spos[1]].value=s;
+            this.state.history[0].squares[po[0]][po[1]].value=sval; //현재 클릭한 칸을 html상에 출력된 선택된 말 정보로 변경(표시값)
+            this.state.history[0].squares[spos[0]][spos[1]].value=''; //html상에 출력된 선택된 말의 원래 좌표의 칸을 비우기
             console.log(this.state.history[0].squares[po[0]][po[1]].value);
             
-            this.state.history[0].squares[po[0]][po[1]].camp=scmp;
-            this.state.history[0].squares[spos[0]][spos[1]].camp=cmp;
+            this.state.history[0].squares[po[0]][po[1]].camp=scmp; //현재 클릭한 칸을 html상에 출력된 선택된 말 정보로 변경(진영)
+            this.state.history[0].squares[spos[0]][spos[1]].camp=''; //html상에 출력된 선택된 말의 원래 좌표의 칸을 비우기
             console.log(this.state.history[0].squares[po[0]][po[1]].camp);
             
-
+            //이동 후 선택된 말 정보 초기화, 플레이어 순서 변경
             this.setState({
                 history: this.state.history,
                 selected:[],
                 next: (this.state.next=='white'? 'black':'white')
             });
-            console.log("next:"+this.state.next);
+            // console.log("next:"+this.state.next);
             sval='';
             spos=[];
             scmp='';
